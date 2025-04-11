9059637496ccd831e8e3db82909f4b7425e2dfca
Andrey Popov <andrvpopov@gmail.com>
2025-04-11 12:01:59 +0300

    Add task06

--- Task04/vendor/composer/installed.php
+++ Task04/vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => '__root__',
         'pretty_version' => 'dev-master',
         'version' => 'dev-master',
-        'reference' => '72b35b7718645953fa2735d5279da5fe0eba2936',
+        'reference' => '9868548d015533cca30694c822ac6003a328785f',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         '__root__' => array(
             'pretty_version' => 'dev-master',
             'version' => 'dev-master',
-            'reference' => '72b35b7718645953fa2735d5279da5fe0eba2936',
+            'reference' => '9868548d015533cca30694c822ac6003a328785f',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
